1c01e40b53ea8eeb430a18c75fdfaaa01a6f4f0c
OWS-Thinhpv <thinhpv@ows.com.vn>
2024-01-14 21:50:17 +0700

    up len naaaaa

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from 'src/app/shared/data/product.data';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  ngOnInit() {}
+  listProduct = products;
+  constructor() {}
+  ngOnInit() {
+    console.log('listProduct', this.listProduct);
+  }
 }
---
+++ src/app/shared/data/product.data.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: '1',
+    name: 'A',
+    title: 'A',
+    decription: 'A',
+    images: '',
+  },
+  {
+    id: '2',
+    name: 'B',
+    title: 'B',
+    decription: 'B',
+    images: '',
+  },
+  {
+    id: '3',
+    name: 'C',
+    title: 'C',
+    decription: 'C',
+    images: '',
+  },
+  {
+    id: '4',
+    name: 'D',
+    title: 'D',
+    decription: 'D',
+    images: '',
+  },
+];
