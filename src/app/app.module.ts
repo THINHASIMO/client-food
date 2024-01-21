0eb7c2911e1c356a25b4f24b61b1e360b80cce53
OWS-Thinhpv <thinhpv@ows.com.vn>
2024-01-21 15:10:45 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { FeaturesModule } from './feature/feature.module';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FeaturesModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, FeaturesModule, SharedModule, BrowserAnimationsModule],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
