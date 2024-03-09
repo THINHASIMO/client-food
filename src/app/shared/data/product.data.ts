c7934c617767ec1056986639453694f0a17cc080
OWS-Thinhpv <thinhpv@ows.com.vn>
2024-03-09 17:00:58 +0700

    main

--- src/app/shared/data/product.data.ts
+++ src/app/shared/data/product.data.ts
@@ -8,174 +8,174 @@ export const products = [
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M02',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M03',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M04',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M05',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M01',
     name: 'tủ gỗ',
     title:
-      ' dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      ' Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M02',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M03',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M04',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M05',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M05',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
   {
     id: 'M05',
     name: 'tủ gỗ',
     title:
-      'dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
+      'Dòng sản phẩm đồ gỗ của chúng tôi <br> sự hoàn hảo từ tỉ mỉ đến hoàn hảo.',
     decription:
       'Chất liệu gỗ gụ là một trong những lựa chọn tối ưu nhất cho nội thất với vẻ đẹp tự nhiên và độ bền vững bẩm sinh. Gỗ gụ được biết đến với đặc tính mịn màng, vân gỗ độc đáo và độ cứng cao, tạo ra sự sang trọng và tinh tế cho mọi không gian. Với sự kết hợp giữa sự độc đáo của vân gỗ và sự cứng cáp của chất liệu, các sản phẩm từ gỗ gụ không chỉ là một điểm nhấn nghệ thuật mà còn là biểu tượng của sự lịch lãm và đẳng cấp trong trang trí nội thất.',
 
     src: 'assets/images/products/1.jpg',
     price: '29,900,000 đ',
-    phone: '0844445970',
+    phone: '0983578862',
     category: 'fish',
     address:
-      'Cửa hàng nội thất Ngọc Hòa, Cơ sở 1 số 22, Đường 99, Thị Trấn Cồn, Huyện Hải Hậu, Nam Định',
+      'Cửa hàng nội thất Hùng Hòa, Xã Hải Minh, Huyện Hải Hậu, Tỉnh Nam Định',
   },
 ];
